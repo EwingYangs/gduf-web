5db6bfe7cf55056eb58a96f5892fa96833462b41
Ewing <jiaying.yang@qq.com>
2018-01-24 23:38:08 +0800

    添加自习室数据

--- gduf-api/controllers/RoomController.php
+++ gduf-api/controllers/RoomController.php
@@ -29,6 +29,12 @@ class RoomController extends BaseController
         $xnxqh = Yii::$app->params['trem'];
         $xqid = Yii::$app->request->post('xqid');//校区
         $jzwid = Yii::$app->request->post('jzwid');//教学楼
+        $jzwidtem = intval($jzwid);
+        if(in_array($jzwid, array(15,16,20,18,19))){
+            //北教
+            $jzwid = 13;
+        }
+
         $gdufclassroomUrl = Yii::$app->params['gdufclassroomUrl'];
         $header = array(
             "Content-Type: application/x-www-form-urlencoded",
@@ -39,20 +45,19 @@ class RoomController extends BaseController
             'jzwid' => $jzwid,
         );
 
-        $cacheKey = 'room' + $jzwid;
+        $cacheKey = 'room' + $jzwidtem;
         $room = Yii::$app->cache->get($cacheKey);//缓存
 
-        if(!$room){
+        if($room){
             $this->roomInfo = $room;
         }else{
             $this->roomInfo = Common::curlPostAppMsg($gdufclassroomUrl, $content, $header, 1800 ,1);
             $this->roomInfo = preg_replace("/[\t\n\r]+/","",$this->roomInfo['query']);//去掉换行、制表等特殊字符
-            $this->roomInfo = GdufFiter::fiterRoom($this->roomInfo); //过滤图书内容
+            $this->roomInfo = GdufFiter::fiterRoom($this->roomInfo); //过滤教室内容
             Yii::$app->cache->set($cacheKey,$this->roomInfo,24*3600*30*5);
         }
-
         $this->roomInfo = $this->selectRoomByDay();
-        $this->roomInfo = $this->fiterFloor($jzwid);//楼层过滤
+        $this->roomInfo = $this->fiterFloor($jzwidtem);//楼层过滤
 
         Common::ajaxResult(State::$SUSSION_CODE , State::$SUSSION_MSG ,$this->roomInfo);
     }
@@ -69,25 +74,44 @@ class RoomController extends BaseController
                 $reg = "/^实验楼(.*)/";
                 return $this->sortsy($reg);
             case 1:
-                //实验楼
+                //2栋
                 $reg = "/^2-(.*)/";
                 return $this->sortsy($reg);
             case 4:
-                //实验楼
+                //1栋
                 $reg = "/^1-(.*)/";
                 return $this->sortsy($reg);
             case 5:
-                //实验楼
-                Common::ajaxResult(State::$SUSSION_CODE , State::$SUSSION_MSG ,$this->roomInfo);
-                $reg = "/^7-(.*)/";
+                //7栋
+                $reg = "/^7－(.*)/";
                 return $this->sortsy($reg);
             case 10:
-                //实验楼
+                //7栋
                 $reg = "/^3-(.*)/";
                 return $this->sortsy($reg);
             case 14:
-                //实验楼
+                //6栋
                 $reg = "/^6-(.*)/";
+                return $this->sortsy($reg);
+            case 15:
+                //北教A
+                $reg = "/^北教A(.*)/";
+                return $this->sortsy($reg);
+            case 16:
+                //北教B
+                $reg = "/^北教B(.*)/";
+                return $this->sortsy($reg);
+            case 20:
+                //北教C
+                $reg = "/^北教C(.*)/";
+                return $this->sortsy($reg);
+            case 18:
+                //北教D
+                $reg = "/^北教D(.*)/";
+                return $this->sortsy($reg);
+            case 19:
+                //北教D
+                $reg = "/^北阶(.*)/";
                 return $this->sortsy($reg);
             default:
                 # code...
